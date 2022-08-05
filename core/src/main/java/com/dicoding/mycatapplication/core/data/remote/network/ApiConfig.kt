08e3c9a7e9c17c487c00505e4bba0390abcfcb30
imranmauludi12 <88386958+imranmauludi12@users.noreply.github.com>
2022-08-05 23:06:46 +0700

    add certificate pinner with OkHttp

--- core/src/main/java/com/dicoding/mycatapplication/core/data/remote/network/ApiConfig.kt
+++ core/src/main/java/com/dicoding/mycatapplication/core/data/remote/network/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.dicoding.mycatapplication.core.data.remote.network
 
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -15,8 +16,15 @@ class ApiConfig {
         val interceptor = HttpLoggingInterceptor()
             .setLevel(HttpLoggingInterceptor.Level.BASIC)
 
+        val certificatePinner = CertificatePinner.Builder()
+            .add(BASE_URL, "sha256//+7UzN/5MaioPqXtGFpX67I2E0VfRkAt5GUs865zp9s=")
+            .add(BASE_URL, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add(BASE_URL, "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
+
         val client = OkHttpClient.Builder()
             .addInterceptor(interceptor)
+            .certificatePinner(certificatePinner)
             .build()
 
         val retrofit = Retrofit.Builder()
